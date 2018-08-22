63571eb0fa0ec5f1355c2709f848710d92ffe913
CJ Melegrito <cj@mlgrto.com>
2018-08-22 15:55:17 +1000

    Basic renaming

---
+++ main.py
@@ -0,0 +1,36 @@
+import rawpy
+import imageio
+
+from pathlib import Path
+
+# Constants
+# -----------------------------
+
+INPUT_FOLDER = 'data/1_100'
+
+# Functions
+# -----------------------------
+
+def get_filenames_from(path):
+	folder = Path(path)
+	return [str(file) for file in folder.iterdir() if file.is_file() and file.suffix == '.CR2']
+
+def generate_output_name(filename):
+	return INPUT_FOLDER + '/output' + filename[len(INPUT_FOLDER):-3] + 'jpg'
+
+def batch_postprocess(images):
+	for image in images:
+		with rawpy.imread(image) as raw:
+			postprocessed = raw.postprocess(gamma=(1,1), no_auto_bright=True, output_bps=8)
+			imageio.imsave(generate_output_name(image), postprocessed)
+
+def main():
+	images = get_filenames_from(INPUT_FOLDER)
+	for image in images:
+		print(generate_output_name(image))
+
+# Start the show
+# -----------------------------
+
+if __name__ == '__main__':
+	main()
